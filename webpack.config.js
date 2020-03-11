58620ff38e9b9a16c4ac92ef6d26625e346fa380
Jesse Bell <jkurtasbell@gmail.com>
2020-03-11 15:08:01 -0700

    Incdlue module Object In webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const path = require('path');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
   }
 }
