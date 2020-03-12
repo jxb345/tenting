a23dc60ac2dfc445f84fffd6ea5253a273b74229
Jesse Bell <jkurtasbell@gmail.com>
2020-03-11 23:15:22 -0700

    Update Scripts In package.json

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hi, from server.js');
-})
+// app.get('/', (req, res) => {
+//   res.send();
+// })
 
 app.listen(port, () => {
   console.log(`listening on ${port}`)})
