5d09450a272f31f177698762dc316aad75370f48
Jesse Bell <jkurtasbell@gmail.com>
2020-03-14 22:34:51 -0700

    Add A Simple Form On App Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ import React from 'react';
 class App extends React.Component {
     render() {
       return (
-      <div>Hi, Tenting</div>
+      <div>
+        <form action="" method="post">
+          <label>NAME: </label>
+          <input type="text" name="name" id="" required/>
+          <br/>
+          <label>PARK: </label>
+          <input type="text" name="name" id="" required/>
+          <br/>
+          <label>CAMPSITE: </label>
+          <input type="text" name="name" id="" required/>
+          <br/>
+          <input type="submit" value="SUBMIT"/>
+        </form>
+      </div>
       )
     }
 }
